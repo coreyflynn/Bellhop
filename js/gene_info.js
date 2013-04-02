d6aada6627937a93f5e1c6bc0b791a4341673b3c
Corey Flynn <flynn.corey@gmail.com>
2013-04-02 11:58:54 -0400

    card view v1 in gene_info.js

--- js/gene_info.js
+++ js/gene_info.js
@@ -3,25 +3,213 @@ function Gene_Info_Object(div_id,width,height,margin){
 	this.width = width;
 	this.height = height;
 	this.margin = margin;
+	this.symbol = "";
+	this.kd_num = 9;
+	this.oe_num = 99;
+	this.kd_num_lines = 3;
+	this.oe_num_lines = 5;
 	this.init = false;
 
 	this.draw = draw;
+	this.draw_bg = draw_bg;
+	this.draw_symbol = draw_symbol;
+	this.draw_KD = draw_KD;
+	this.draw_OE = draw_OE;
+	this.update_symbol = update_symbol;
+	this.update_long_name = update_long_name;
+	this.update_kd = update_kd;
+	this.update_oe = update_oe;
 
-	function draw(gene_name){
+	function draw(symbol,width){
 		if (!this.init){
-			this.svg=d3.select(this.div_id).append("svg").attr("width",width).attr("height",height);
+			this.svg=d3.select(this.div_id).append("svg")
+										   .attr("width",width)
+										   .attr("height",height);
+			this.init = true;
 		}
+		this.width = width;
+		var x=d3.scale.linear().domain([0,1]).range([this.margin,this.width-this.margin]);
+		var y=d3.scale.linear().domain([0,1]).range([this.height-this.margin,this.margin]);
+		this.draw_bg(x,y,this.width);
 
-		var symbol = this.svg.selectAll("text")
-						.data([gene_name]);
+		var self = this;
+		var geneinfo = 'http://lincscloud.org/api/geneinfo?callback=?';
+		$.getJSON(geneinfo,{q:'{"pr_gene_symbol":"' + symbol + '"}'},
+                          function(response){
+                            self.update_symbol(response[0].pr_gene_symbol);
+                            self.update_long_name(response[0].pr_gene_title);
+                            self.draw_symbol(x,y);
+                          }
+        );
+        var siginfo = 'http://lincscloud.org/api/siginfo?callback=?';
+        $.getJSON(siginfo,{q:'{"pert_desc":"' + symbol +'","pert_type":"trt_sh"}',
+						  f:'{"cell_id":1}',
+						  l:1000},
+                          function(response){
+                          	var cells = []
+                          	response.forEach(function(element,index,array){
+						        	cells.push(element.cell_id);
+					        });
+					        cells = _.uniq(cells);
+                            self.update_kd(response.length,cells.length);
+                    		self.draw_KD(x,y);
+                          }
+        );
 
-		x=d3.scale.linear().domain([0,1]).range([this.margin,this.width-this.margin]);
-		y=d3.scale.linear().domain([0,1]).range([this.height-this.margin,this.margin]);
+        $.getJSON(siginfo,{q:'{"pert_desc":"' + symbol +'","pert_type":"trt_oe"}',
+						  f:'{"cell_id":1}',
+						  l:1000},
+                          function(response){
+                          	var cells = []
+                          	response.forEach(function(element,index,array){
+						        	cells.push(element.cell_id);
+					        });
+					        cells = _.uniq(cells);
+                            self.update_oe(response.length,cells.length);
+                    		self.draw_OE(x,y);
+                          }
+        );
 		
-		symbol.enter().append("text")
-			.attr("x",x(0.5))
-			.attr("y",x(0.5))
-			.text(d);
+	}
+		
+	function update_symbol(symbol){
+		this.symbol = symbol;
+	}
+	
+	function update_long_name(long_name){
+		this.long_name = long_name;
+	}
+
+	function update_kd(kd_num,kd_num_lines){
+		this.kd_num = kd_num;
+		this.kd_num_lines = kd_num_lines;
+	}	
+	
+	function update_oe(oe_num,oe_num_lines){
+		this.oe_num = oe_num;
+		this.oe_num_lines = oe_num_lines;
+	}
+
+	function draw_bg(x,y,width){
+		this.svg.selectAll("rect.bg").data([]).exit().remove();
+		
+		this.svg.selectAll("rect.bg").data([1])
+				.enter().append("rect")
+				.attr("x",x(0) - margin)
+				.attr("y",y(1) - margin)
+				.attr("class","bg")
+				.attr("height", height)
+				.attr("width", width)
+				.attr("fill", "#f0f0f0");
+	}
+
+	function draw_symbol(x,y){
+		this.svg.selectAll("text.symbol").data([]).exit().remove();
+
+		this.svg.selectAll("text.symbol").data([this.symbol])
+			.enter().append("text")
+			.attr("class","symbol")
+			.attr("x",x(0))
+			.attr("y",y(0.9))
+			.attr("font-size",40)
+			.text(function(d){ return d;});
+
+		// long name text element
+		this.svg.selectAll("text.long").data([]).exit().remove();
+
+		this.svg.selectAll("text.long").data([this.long_name])
+			.enter().append("text")
+			.attr("class","long")
+			.attr("x",x(0))
+			.attr("y",y(0.9) + 20)
+			.attr("font-size",20)
+			.text(function(d){ return d;});
+	}
+
+	function draw_KD(x,y){
+		this.svg.selectAll("image.down").data([]).exit().remove();
+		this.svg.selectAll("image.down").data([this.kd_num])
+			.enter().append("image")
+			.attr("xlink:href","http://coreyflynn.github.com/Bellhop/img/arrow_down_round_small.png")
+			.attr("class","down")
+			.attr("x",x(0) + 50)
+			.attr("y",y(0.6))
+			.attr("height",100)
+			.attr("width",100);
+
+		this.svg.selectAll("text.kd_text").data([]).exit().remove();
+		this.svg.selectAll("text.kd_text").data([this.kd_num])
+			.enter().append("text")
+			.attr("class","kd_text")
+			.attr("x",x(0) + 100)
+			.attr("y",y(0.6) + 60)
+			.attr("font-size",25)
+			.attr("text-anchor","middle")
+			.attr("fill","#f0f0f0")
+			.text(this.kd_num);
+
+		this.svg.selectAll("text.down").data([]).exit().remove();
+		this.svg.selectAll("text.down").data([this.kd_num])
+			.enter().append("text")
+			.attr("class","down")
+			.attr("x",x(0) + 100)
+			.attr("y",y(0.6) + 115)
+			.attr("font-size",20)
+			.attr("text-anchor","middle")
+			.text("KD Experiments");
+
+		this.svg.selectAll("text.kd_lines_text").data([]).exit().remove();
+		this.svg.selectAll("text.kd_lines_text").data([this.kd_num])
+			.enter().append("text")
+			.attr("class","kd_lines_text")
+			.attr("x",x(0) + 100)
+			.attr("y",y(0.6) + 135)
+			.attr("font-size",20)
+			.attr("text-anchor","middle")
+			.text("In " + this.kd_num_lines + " Cell Lines");
+	}
+
+	function draw_OE(x,y){
+		this.svg.selectAll("image.up").data([]).exit().remove();
+		this.svg.selectAll("image.up").data([this.kd_num])
+			.enter().append("image")
+			.attr("xlink:href","http://coreyflynn.github.com/Bellhop/img/arrow_up_round_small.png")
+			.attr("class","up")
+			.attr("x",x(1) - 150)
+			.attr("y",y(0.6))
+			.attr("height",100)
+			.attr("width",100);
+
+		this.svg.selectAll("text.oe_text").data([]).exit().remove();
+		this.svg.selectAll("text.oe_text").data([this.oe_num])
+			.enter().append("text")
+			.attr("class","oe_text")
+			.attr("x",x(1) - 100)
+			.attr("y",y(0.6) + 60)
+			.attr("font-size",25)
+			.attr("text-anchor","middle")
+			.attr("fill","#f0f0f0")
+			.text(this.oe_num);
+
+		this.svg.selectAll("text.up").data([]).exit().remove();
+		this.svg.selectAll("text.up").data([this.oe_num])
+			.enter().append("text")
+			.attr("class","up")
+			.attr("x",x(1) - 100)
+			.attr("y",y(0.6) + 115)
+			.attr("font-size",20)
+			.attr("text-anchor","middle")
+			.text("OE Experiments");
+
+		this.svg.selectAll("text.oe_lines_text").data([]).exit().remove();
+		this.svg.selectAll("text.oe_lines_text").data([this.kd_num])
+			.enter().append("text")
+			.attr("class","oe_lines_text")
+			.attr("x",x(1) - 100)
+			.attr("y",y(0.6) + 135)
+			.attr("font-size",20)
+			.attr("text-anchor","middle")
+			.text("In " + this.oe_num_lines + " Cell Lines");
 	}
 
 }
