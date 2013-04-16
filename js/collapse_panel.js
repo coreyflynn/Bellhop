50906838a16654e98cca6a4f3c697bb57d7c9d5a
Corey Flynn <flynn.corey@gmail.com>
2013-04-16 10:28:57 -0400

    popover_label changes

--- js/collapse_panel.js
+++ js/collapse_panel.js
@@ -299,7 +299,7 @@ function SC_Panel(options){
 			.attr("width", 600)
 			.attr("height", 30)
 			.append("xhtml:div")
-			.html('<span class="label pop_over_label" style:></span><span class="label" style:>foo</span>');
+			.html('<span class="label pop_over_label" style:></span><div></div><span class="label" style:>foo</span>');
 
 	}
 
