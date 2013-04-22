1682acf8f7db301d3ba39e0a6410cdcf3d0a1409
Corey Flynn <flynn.corey@gmail.com>
2013-04-22 15:06:25 -0400

    tile.js

--- js/tile.js
+++ js/tile.js
@@ -109,8 +109,7 @@ Tile.prototype.draw_bg = function() {
 	// (re)draw the background
 	this.svg.selectAll("rect.bg").data([]).exit().remove();
 	this.svg.selectAll("rect.bg").data([1])
-			.enter().append("a")
-			.attr("xlink:href",this.link)
+			.enter()
 			.append("rect")
 			.attr("x",0)
 			.attr("y",0)
@@ -119,7 +118,9 @@ Tile.prototype.draw_bg = function() {
 			.attr("class","bg")
 			.attr("height", this.height)
 			.attr("width", this.width)
-			.attr("fill", this.color);
+			.attr("fill", this.color)
+			.append("a")
+			.attr("xlink:href",this.link);
 };
 
 /**
