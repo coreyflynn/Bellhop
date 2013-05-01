fa2e983df0e6e66098eb46c978b2b92258dda72c
Corey Flynn <flynn.corey@gmail.com>
2013-05-01 10:56:17 -0400

    collapse_panel hover

--- js/collapse_panel.js
+++ js/collapse_panel.js
@@ -45,7 +45,7 @@ Collapse_Panel.prototype.init = function(){
 	$(this.div_target).css("padding-bottom", "2px");
 
 	$("#" + this.div_id + "_top_panel").click(function (evt) { self.buttonCallback(); });
-	$("#" + this.div_id + "_top_panel").hover(function () { $("#" + this.div_id).css("background-color","#d0d0d0"); },
+	$("#" + this.div_id + "_top_panel").hover(function () { $("#" + this.div_id).css("background-color","#bdbdbd"); },
 											  function () { $("#" + this.div_id).css("background-color","#f0f0f0"); });
 
 	this.close_height = $("#" + this.div_id).outerHeight();
