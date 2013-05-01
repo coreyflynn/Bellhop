e010b5ab2f67c7ab399647c0502c794a2ae1b782
Corey Flynn <flynn.corey@gmail.com>
2013-05-01 11:10:26 -0400

    red hover

--- js/collapse_panel.js
+++ js/collapse_panel.js
@@ -44,8 +44,9 @@ Collapse_Panel.prototype.init = function(){
 	$(this.div_target).append(this.html);
 	$(this.div_target).css("padding-bottom", "2px");
 
-	$("#" + this.div_id + "_top_panel").click(function (evt) { self.buttonCallback(); });
 	$("#" + this.div_id + "_top_panel").mouseenter(function (evt) { $("#" + this.div_id).css("background-color","red"); });
+	$("#" + this.div_id + "_top_panel").click(function (evt) { self.buttonCallback(); });
+	
 
 	this.close_height = $("#" + this.div_id).outerHeight();
 	this.open_height = $("#" + this.div_id).outerHeight();
